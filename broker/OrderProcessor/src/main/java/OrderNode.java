18d878664e9d92f0af00a82471a3608bea66d80d
gyh <RemGYH@sjtu.edu.cn>
2019-05-16 22:26:03 +0800

    order node (list) with lock

--- broker/OrderProcessor/src/main/java/OrderNode.java
+++ broker/OrderProcessor/src/main/java/OrderNode.java
@@ -1,22 +1,39 @@
-/***********************************************************************
- * Module:  OrderNode.java
- * Author:  gyh
- * Purpose: Defines the Class OrderNode
- ***********************************************************************/
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-import java.util.*;
-
-/** @pdOid 933eaceb-f4e9-4b11-9004-56a609bf1102 */
 public class OrderNode {
-   /** @pdOid ef65e852-de20-4223-8c96-90a549cfcbb8 */
-   private Order order;
-   /** @pdOid 7b2e2982-9b7b-4722-a9ec-558e67dffe63 */
-   private OrderNode next;
-   
-   /** @pdOid 5bdd4ff2-e74a-4a04-8639-a01f71c996bc */
-   public Order getOrder() {
-      // TODO: implement
-      return null;
-   }
+
+    private Order order;
+    private OrderNode next;
+    private Lock lock = new ReentrantLock();
+
+    public OrderNode(Order order) {
+        this.order = order;
+        this.next = null;
+    }
+
+    public void lock() {
+        this.lock.lock();
+    }
+
+    public void unlock() {
+        this.lock.unlock();
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public OrderNode getNext() {
+        return next;
+    }
+
+    public void setNext(OrderNode next) {
+        this.next = next;
+    }
 
 }
--- broker/OrderProcessor/src/main/java/OrderNodeList.java
+++ broker/OrderProcessor/src/main/java/OrderNodeList.java
@@ -1,40 +1,95 @@
-/***********************************************************************
- * Module:  OrderNodeList.java
- * Author:  gyh
- * Purpose: Defines the Class OrderNodeList
- ***********************************************************************/
 
-import java.util.*;
-
-/** @pdOid c1d1c622-86b1-46df-9188-02b67c11bdbc */
 public class OrderNodeList {
-   /** @pdOid e4111b6d-6364-4ad7-94e8-35f9ef20031a */
-   private OrderNode head;
-   /** @pdOid b249c287-ae0a-4c02-9e8c-a42567a49972 */
-   private OrderNode tail;
-   
-   /** @pdOid d6f7ce6e-dab3-48fb-b47c-bcff3cf73723 */
-   public Boolean add() {
-      // TODO: implement
-      return null;
-   }
-   
-   /** @pdOid 48a14204-77cc-4820-8160-d0a6ae61078a */
-   public Order cancelOrder() {
-      // TODO: implement
-      return null;
-   }
-   
-   /** @pdOid 5d985f1e-5895-4d86-a8c4-7b9f911f14d4 */
-   public Order candidateOrder() {
-      // TODO: implement
-      return null;
-   }
-   
-   /** @pdOid 4b627e48-d295-4c28-9ca6-f15dc290a8fb */
-   public Boolean removeOrder() {
-      // TODO: implement
-      return null;
-   }
+
+    private final OrderNode head = new OrderNode(null);
+    private OrderNode tail = this.head;
+
+    public Boolean isEmpty() {
+        this.head.lock();
+        Boolean isEmpty = (this.head.getNext() == null);
+        this.head.unlock();
+        return isEmpty;
+    }
+
+    public Boolean add(Order order) {
+        OrderNode node = new OrderNode(order);
+        // lock & append
+        this.tail.lock();
+        OrderNode oldTail = this.tail;
+        {
+            oldTail.setNext(node);
+            this.tail = node;
+        }
+        oldTail.unlock();
+        return true;
+    }
+
+    public Order cancelOrder(Order cancelOrder) {
+        OrderNode node = this.head;
+        OrderNode prev;
+        node.lock();
+        while (true) {
+            if (node.getNext() != null) {
+                prev = node;
+                node = prev.getNext();
+                Order order = node.getOrder();
+                order.lock();
+                if (order.getOrderId().equals(cancelOrder.getCancelId())) {
+                    node.lock();
+                    prev.setNext(node.getNext());
+                    prev.unlock();
+                    node.unlock();
+                    order.unlock();
+                    return order;
+                }
+                order.unlock();
+                node.lock();
+                prev.unlock();
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public Order candidateOrder() {
+        this.head.lock();
+        OrderNode firstNode = this.head.getNext();
+        if (firstNode != null) {
+            firstNode.lock();
+            Order order = firstNode.getOrder();
+            order.lock();
+            firstNode.unlock();
+            this.head.unlock();
+            return order;
+        }
+        return null;
+    }
+
+    public Boolean removeOrder(Order toRemove) {
+        OrderNode node = this.head;
+        OrderNode prev;
+        node.lock();
+        while (true) {
+            if (node.getNext() != null) {
+                prev = node;
+                node = prev.getNext();
+                Order order = node.getOrder();
+                order.lock();
+                if (order == toRemove) {
+                    node.lock();
+                    prev.setNext(node.getNext());
+                    prev.unlock();
+                    node.unlock();
+                    order.unlock();
+                    return true;
+                }
+                order.unlock();
+                node.lock();
+                prev.unlock();
+            } else {
+                return false;
+            }
+        }
+    }
 
 }
