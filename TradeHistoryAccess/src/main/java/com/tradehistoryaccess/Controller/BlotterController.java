ee8ced57ce775c39a13de55bec3e429e83d6ed9b
yssjtu <635664803@qq.com>
2019-06-07 16:24:39 +0800

    controller

--- TradeHistoryAccess/src/main/java/com/tradehistoryaccess/Controller/BlotterController.java
+++ TradeHistoryAccess/src/main/java/com/tradehistoryaccess/Controller/BlotterController.java
@@ -12,10 +12,10 @@ import java.util.List;
 
 @RestController
 public class BlotterController {
-    @CrossOrigin(origins = "http://localhost:3000",maxAge = 3600)
+    @CrossOrigin(origins = "*",maxAge = 3600)
     @RequestMapping(value = "/history",method = RequestMethod.GET)
     public String getHistory(
-            @RequestParam(value = "productid") String productid,@RequestParam(value = "period") String period,@RequestParam(value = "starttime",required = false,defaultValue = "0") String starttime,@RequestParam(value = "endtime",required = false,defaultValue = "999999999999999") String endtime,@RequestParam(value = "name",required = false,defaultValue = "") String name){
+            @RequestParam(value = "productid") String productid,@RequestParam(value = "period") String period,@RequestParam(value = "starttime",required = false,defaultValue = "0") String starttime,@RequestParam(value = "endtime",required = false,defaultValue = "999999999999999") String endtime,@RequestParam(value = "corpname",required = false,defaultValue = "magic") String corpName){
 
         System.out.println("id: "+productid+" period "+period+" starttime "+starttime+" endtime "+endtime);
         Session s= HibernateUtils.getCurrentSession();
@@ -30,6 +30,20 @@ public class BlotterController {
 
         List<TradeDTO> trades=q.list();
         tr.commit();
+        String corpname= corpName;
+        if(!corpname.equals("magic")) {
+
+
+            for (TradeDTO tradeDTO : trades) {
+                if (tradeDTO.getInitTrader().equals(corpName) || tradeDTO.getCompCompany().equals(corpName)) {
+                    continue;
+                }
+                tradeDTO.setInitCompany("");
+                tradeDTO.setInitTrader("");
+                tradeDTO.setCompCompany("");
+                tradeDTO.setCompTrader("");
+            }
+        }
         Gson gson=new Gson();
         return gson.toJson(trades);
 
